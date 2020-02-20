6bc73ba94326f4569abd15cfec77d0dd1a0f5756
Heather Logan <2128536l@student.gla.ac.uk>
2020-02-20 12:40:33 +0000

    adds tweets to mongodb

--- crawler_stream.py
+++ crawler_stream.py
@@ -1,61 +1,66 @@
 from __future__ import print_function
 import tweepy
 import json
+import pymongo
 from pymongo import MongoClient
 
 
-client = MongoClient()
-db = client.tweet_db
-twitterdb = db.tweet_collection
-
-MONGO_HOST= 'mongodb://localhost/twitterdb'  # assuming you have mongoDB installed
-
-WORDS = ['#bigdata', '#AI', '#datascience', '#machinelearning', '#ml', '#iot']
+# TWITTER API Authentication
 
 CONSUMER_KEY = "TYPVBk4kO17UMfDZSOURAGL4E"
 CONSUMER_SECRET = "0Kq5zHsOb4lNfDGGbwOmhVaXTkpwS1DZiA5kBgpFt6L3OilJdt"
 ACCESS_TOKEN = "431764921-CaJPtXrUEu6PoYsyYozOFMrAu7rbSBZKUOppVpBq"
 ACCESS_TOKEN_SECRET = "N1qZUsVN5h4vz31MdIAwJWIkO6oJCJUBx6PSlIXRvKqSj"
 
-class StreamListener(tweepy.StreamListener):
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+# Database Setup
+
+client=MongoClient()
+db=client.tweet_db
+tweet_collection = db.tweet_collection
+# tweet_collection.create_index([('id',pymongo.ASCENDING)], unique=True )
+print('Database created')
+
+
+class StdOutListener(tweepy.StreamListener):
     # This is a class provided by tweepy to access the Twitter Streaming API.
 
+    def __init__(self):
+        super().__init__()
+        self.counter = 0
+        self.limit = 10
+
     def on_connect(self):
-        # Called initially to connect to the Streaming API
-        print("You are now connected to the streaming API.")
+        print("Connected")
+
 
     def on_error(self, status_code):
-        # On error - if an error occurs, display the error / status code
-        print('An Error has occured: ' + repr(status_code))
+        print('Error: ' + repr(status_code))
         return False
 
     def on_data(self, data):
-        # This is the meat of the script...it connects to your mongoDB and stores the tweet
         try:
-            client = MongoClient(MONGO_HOST)
-
-            # Use twitterdb database. If it doesn't exist, it will be created.
-            db = client.twitterdb
-
-            # Decode the JSON from Twitter
             datajson = json.loads(data)
+            tweet_collection.insert(datajson)
+            self.counter += 1
+            if self.counter < self.limit:
+                return True
+            else:
+                streamer.disconnect()
 
-            # grab the 'created_at' data from the Tweet to use for display
-            created_at = datajson['created_at']
-
-            # print out a message to the screen that we have collected a tweet
-            print("Tweet collected at " + str(created_at))
-
-            # insert the data into the mongoDB into a collection called twitter_search
-            # if twitter_search doesn't exist, it will be created.
-            db.twitter_search.insert(datajson)
         except Exception as e:
             print(e)
 
+if __name__=='__main__':
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-listener = StreamListener(api=tweepy.API(wait_on_rate_limit=True))
-streamer = tweepy.Stream(auth=auth, listener=listener)
-print("Tracking: " + str(WORDS))
-streamer.filter(track=WORDS)
+    listener = StdOutListener()
+    streamer = tweepy.Stream(auth=auth, listener=listener)
+
+    WORDS = ['boris johnson', 'jeremy corbyn', 'nicola sturgeon']
+    streamer.filter(track=WORDS)
+
+    cursor = tweet_collection.find()
+    print(cursor.count())
+    print(tweet_collection.distinct("user.id"))
