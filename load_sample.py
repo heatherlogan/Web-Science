85d675415e78d1cfb9816b60065a1a77ab14e284
Heather Logan <2128536l@student.gla.ac.uk>
2020-03-20 11:54:34 +0000

    fixes3

---
+++ load_sample.py
@@ -0,0 +1,48 @@
+import json
+import pymongo
+from json import JSONEncoder
+
+from bson import ObjectId
+from pymongo import MongoClient
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+def mongo_to_csv():
+
+    client = MongoClient()
+    db = client.tweet_db
+    streaming_tweets = db.streaming_tweets
+    rest_tweets = db.rest_tweets
+    print('Database created')
+
+    cursor1 = streaming_tweets.find()
+    cursor2 = rest_tweets.find()
+    cursor = list(cursor1) + list(cursor2)
+
+    file = open("sample_tweets.json", "w")
+    file.write('[')
+
+    for document in cursor[:10]:
+        stringdoc = json.dumps(document, cls=JSONEncoder)
+        file.write(stringdoc)
+        file.write(',\n')
+    file.write(']')
+
+
+
+def load_csv_to_mongo():
+
+    with open('sample_tweets.json') as f:
+        tweetdb = json.load(f)
+
+    return tweetdb
+
+
+if __name__=='__main__':
+
+    mongo_to_csv()
--- offensive_lang.py
+++ offensive_lang.py
@@ -268,6 +268,10 @@ def get_indexed_tweets(list_tweets, index_list):
 
     return filtered
 
+def tokenize_join(tweet):
+    tweet = str(tweet).strip().split()
+    return ' '.join(tweet)
+
 
 
 if __name__=="__main__":
@@ -348,6 +352,7 @@ if __name__=="__main__":
         task_b_df['text'] = offensive_tweets
         task_b_df['Task B'] = task_b_results
         task_b_df = task_b_df.drop_duplicates('text')
+        task_b_df.index.name = 'idx'
         task_b_df.to_csv('data/task3/tweets/subtask_B_tweets.csv')
 
     print('\n\n')
@@ -373,6 +378,36 @@ if __name__=="__main__":
         task_c_df['text'] = targeted_insults
         task_c_df['Task C'] = task_c_results
         task_c_df = task_c_df.drop_duplicates('text')
+        task_c_df.index.name = 'idx'
         task_c_df.to_csv('data/task3/tweets/subtask_C_tweets.csv')
 
+        # ROUGH FIX FOR TEMPORARY FILES
 
+        pp_tweets = pd.read_csv('data/preprocessed_tweets.csv')
+        a_results = pd.read_csv('data/task3/tweets/subtask_A_tweets.csv')
+        b_results = pd.read_csv('data/task3/tweets/subtask_B_tweets.csv')
+        b_results = b_results.set_index('idx')
+        b_results = b_results.drop_duplicates('text')
+        c_results = pd.read_csv('data/task3/tweets/subtask_C_tweets.csv')
+        c_results = c_results.set_index('idx')
+
+        pp_tweets['text'] = pp_tweets['text'].apply(tokenize_join)
+        b_results['text'] = b_results['text'].apply(tokenize_join)
+        task_c_df['text'] = task_c_df['text'].apply(tokenize_join)
+
+        merged1 = pd.merge(pp_tweets, b_results, on='text')
+        merged1 = merged1.drop_duplicates('text')
+
+        merged2 = pd.merge(merged1, c_results, on='text')
+        merged2 = merged2.drop_duplicates('text')
+
+        merged1 = merged1.drop('text', axis=1)
+        merged2 = merged2.drop('text', axis=1)
+
+        full_tweets_B = pd.merge(a_results, merged1, on='tweet_id')
+        full_tweets_B = full_tweets_B.drop_duplicates('tweet_id')
+        full_tweets_B.to_csv('data/task3/tweets/subtask_B_tweets.csv')
+
+        full_tweets_C = pd.merge(a_results, merged2, on='tweet_id')
+        full_tweets_C = full_tweets_C.drop_duplicates('tweet_id')
+        full_tweets_C.to_csv('data/task3/tweets/subtask_C_tweets.csv')
